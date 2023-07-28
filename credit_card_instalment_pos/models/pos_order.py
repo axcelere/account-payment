227c84593a065f1e2e30cab399a8c7ca4ea22f70
ernesto <ernesto@axcelere.com>
2023-07-28 10:14:53 -0400

    [UPD] Updating payway registry to vat

--- credit_card_instalment_pos/models/pos_order.py
+++ credit_card_instalment_pos/models/pos_order.py
@@ -23,5 +23,51 @@ class PosOrder(models.Model):
         payment["tiket_number"] = ui_paymentline.get("tiket_number", False)
         payment["lot_number"] = ui_paymentline.get("lot_number", False)
         payment["fee"] = ui_paymentline.get("fee", False)
-
         return payment
+
+
+class PosSession(models.Model):
+    _inherit = 'pos.session'
+
+    def _loader_params_pos_payment_method(self):
+        return {
+            'search_params': {
+                'domain': [('card_id', '!=', False)],
+                'fields': [
+                    'card_id', 'instalment_ids', 'instalment_product_id',
+                ],
+            }
+        }
+
+    def _get_pos_ui_pos_payment_method_by_params(self, custom_search_params):
+        """
+        :param custom_search_params: a dictionary containing params of a search_read()
+        """
+        params = self._loader_params_pos_payment_method()
+        # custom_search_params will take priority
+        params['search_params'] = {**params['search_params'], **custom_search_params}
+        payment = self.env['pos.payment.method'].search_read(**params['search_params'])
+        return payment
+
+    def _loader_params_account_card_instalment(self):
+        return {
+            'search_params': {
+                'fields': [
+                    'card_id', 'name', 'instalment', 'product_id', 'amount', 'coefficient', 'discount', 'bank_discount', 'active', 'card_type'
+                ],
+            }
+        }
+
+    def _get_pos_ui_account_card_instalment_by_params(self, custom_search_params):
+        """
+        :param custom_search_params: a dictionary containing params of a search_read()
+        """
+        params = self._loader_params_account_card_instalment()
+        # custom_search_params will take priority
+        params['search_params'] = {**params['search_params'], **custom_search_params}
+        payment = self.env['account.card.instalment'].search_read(**params['search_params'])
+        return payment
+
+
+
+
