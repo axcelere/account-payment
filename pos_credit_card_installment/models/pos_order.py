6764b3d71407a271d899ac82ba3f8ec06d6fe59a
ernesto <ernesto@axcelere.com>
2024-09-09 23:03:32 -0400

    [UPD][pos_credit_card_installment]: Updating installment and surcharge

--- pos_credit_card_installment/models/pos_order.py
+++ pos_credit_card_installment/models/pos_order.py
@@ -8,13 +8,12 @@ _logger = logging.getLogger(__name__)
 class PosOrder(models.Model):
     _inherit = "pos.order"
 
-    # @api.model
-    # def create_from_ui(self, orders, draft=False):
-    #     _logger.info(orders)
-    #     installment_id = self.env['account.card.installment'].search([('id', '=', orders[0]['data']['installment'])])
-    #     ctx = dict(self.env.context or {})
-    #     ctx.update({'installment_id': installment_id})
-    #     return super(PosOrder, self.with_context(ctx)).create_from_ui(orders, draft)
+    @api.model
+    def _order_fields(self, ui_order):
+        res = super(PosOrder, self)._order_fields(ui_order)
+        if ui_order.get("installment_id"):
+            res.update({"installment_id": ui_order["installment_id"]})
+        return res
 
     @api.model
     def _payment_fields(self, order, ui_paymentline):
@@ -29,38 +28,36 @@ class PosOrder(models.Model):
     @api.model
     def _process_order(self, order, draft, existing_order):
         res = super(PosOrder, self)._process_order(order, draft, existing_order)
+        order_id = self.env['pos.order'].search([('id', '=', res)])
+        payment_method_id = order['data']['statement_ids'][0][2]['payment_method_id']
+        installment_id = self.env['account.card.installment'].search([('id', '=', order['data']['installment_id'])])
+        if installment_id and installment_id.surcharge_coefficient:
+            surcharge_coefficient = order['data']['amount_total'] * installment_id.surcharge_coefficient
+            payment_id = self.env['pos.make.payment'].with_context(active_id=order_id.id).create({'amount':surcharge_coefficient, 'payment_method_id': payment_method_id})
+            payment_id.check()
         return res
-        # order_id = self.env['pos.order'].search([('id', '=', res)])
-        # payment_method_id = order['data']['statement_ids'][0][2]['payment_method_id']
-        # installment_id = self.env['account.card.installment'].search([('id', '=', order['data']['installment'])])
-        # if installment_id and installment_id.surcharge_coefficient:
-        #     surcharge_coefficient = order['data']['amount_total'] * installment_id.surcharge_coefficient
-        #     payment_id = self.env['pos.make.payment'].with_context(active_id=order_id.id).create({'amount':surcharge_coefficient, 'payment_method_id': payment_method_id})
-        #     payment_id.check()
-        # return res
 
     @api.model
     def _order_fields(self, ui_order):
         res = super(PosOrder, self)._order_fields(ui_order)
+        installment_id = self.env['account.card.installment'].search([('id', '=', ui_order['installment_id'])])
+        if installment_id and installment_id.surcharge_coefficient:
+            surcharge_coefficient = res['amount_total'] * installment_id.surcharge_coefficient
+            product_id = self.env['product.product'].search([('is_financial_charge', '=', True)])
+            pos_session_id = self.env['pos.session'].search([('id', '=', ui_order['pos_session_id'])])
+            res['amount_total'] += surcharge_coefficient
+            res['amount_paid'] += surcharge_coefficient
+            res['lines'].append((0, 0, {
+                'name': pos_session_id.name,
+                'full_product_name': product_id.name,
+                'price_unit': surcharge_coefficient,
+                'product_id': product_id.id,
+                'qty': 1,
+                'discount': 0,
+                'price_extra': 1,
+                'price_subtotal': surcharge_coefficient,
+                'price_subtotal_incl': surcharge_coefficient,
+                'tax_ids': [],
+                'pack_lot_ids': [],
+            }))
         return res
-        # installment_id = self.env['account.card.installment'].search([('id', '=', ui_order['installment'])])
-        # if installment_id and installment_id.surcharge_coefficient:
-        #     surcharge_coefficient = res['amount_total'] * installment_id.surcharge_coefficient
-        #     product_id = self.env['product.product'].search([('is_financial_charge', '=', True)])
-        #     pos_session_id = self.env['pos.session'].search([('id', '=', ui_order['pos_session_id'])])
-        #     res['amount_total'] += surcharge_coefficient
-        #     res['amount_paid'] += surcharge_coefficient
-        #     res['lines'].append((0, 0, {
-        #         'name': pos_session_id.name,
-        #         'full_product_name': product_id.name,
-        #         'price_unit': surcharge_coefficient,
-        #         'product_id': product_id.id,
-        #         'qty': 1,
-        #         'discount': 0,
-        #         'price_extra': 1,
-        #         'price_subtotal': surcharge_coefficient,
-        #         'price_subtotal_incl': surcharge_coefficient,
-        #         'tax_ids': [],
-        #         'pack_lot_ids': [],
-        #     }))
-        # return res
--- pos_credit_card_installment/static/src/overrides/models/pos_store.js
+++ pos_credit_card_installment/static/src/overrides/models/pos_store.js
@@ -7,15 +7,24 @@
 
 import { PosStore } from "@point_of_sale/app/store/pos_store";
 import { patch } from "@web/core/utils/patch";
-// import { Order } from "@point_of_sale/app/store/models";
+import { Order } from "@point_of_sale/app/store/models";
 
-// patch(Order.prototype, {
-//
-//     setup(order) {
-//         super.setup(...arguments);
-//         this.installment_id = order.installment_id;
-//     },
-// });
+patch(Order.prototype, {
+
+    setup(_defaultObj, options) {
+        super.setup(...arguments);
+        this.installment_id = this.installment_id || options.installment_id;
+    },
+    init_from_JSON(json) {
+        super.init_from_JSON(...arguments);
+        this.installment_id = json.installment_id;
+    },
+    export_as_JSON() {
+        const json = super.export_as_JSON(...arguments);
+        json.installment_id = this.installment_id;
+        return json;
+    },
+});
 
 patch(PosStore.prototype, {
 
@@ -40,108 +49,4 @@ patch(PosStore.prototype, {
         }
         console.log('Installment selected:', this.pos.installment_id);
     },
-
-    // async _flush_orders(orders, options = {}) {
-    //     console.log('_flush_orders', orders);
-    //     console.log(orders[0][0]['data']);
-    //     try {
-    //         const server_ids = await this._save_to_server(orders, options);
-    //         for (let i = 0; i < server_ids.length; i++) {
-    //             this.validated_orders_name_server_id_map[server_ids[i].pos_reference] =
-    //                 server_ids[i].id;
-    //         }
-    //         return server_ids;
-    //     } catch (error) {
-    //         if (!(error instanceof ConnectionLostError) && !options.printedOrders) {
-    //             for (const order of orders) {
-    //                 const reactiveOrder = this.orders.find((o) => o.uid === order.id);
-    //                 reactiveOrder.finalized = false;
-    //                 this.db.remove_order(reactiveOrder.uid);
-    //                 this.db.save_unpaid_order(reactiveOrder);
-    //             }
-    //             this.set_synch("connected");
-    //         }
-    //         throw error;
-    //     } finally {
-    //         this._after_flush_orders(orders);
-    //     }
-    // },
-
-    async _save_to_server(orders, options) {
-        if (!orders || !orders.length) {
-            return Promise.resolve([]);
-        }
-        this.set_synch("connecting", orders.length);
-        options = options || {};
-
-        // Keep the order ids that are about to be sent to the
-        // backend. In between create_from_ui and the success callback
-        // new orders may have been added to it.
-        var order_ids_to_sync = orders.map((o) => o.id);
-
-
-        for (const order of orders) {
-            console.log("AQUI OBTENGO LA ORDEN ANTES DE")
-            // const installment = this.env.pos.installment
-            console.log(order)
-            console.log('ADD INSTALLMENT')
-            // console.log(installment)
-            // order['']
-            //  console.log('ADD INSTALLMENT')
-            // console.log(order[0][0])
-            order.to_invoice = options.to_invoice || false;
-            order['data']['installment'] = '1'
-            // order.installment = this.pos.installment || false;
-        }
-        // we try to send the order. silent prevents a spinner if it takes too long. (unless we are sending an invoice,
-        // then we want to notify the user that we are waiting on something )
-        const orm = options.to_invoice ? this.orm : this.orm.silent;
-        console.log("Testing")
-        console.log(orders)
-        console.log(options)
-
-        try {
-            // FIXME POSREF timeout
-            // const timeout = typeof options.timeout === "number" ? options.timeout : 30000 * orders.length;
-            const serverIds = await orm.call(
-                "pos.order",
-                "create_from_ui",
-                [orders, options.draft || false],
-                {
-                    context: this._getCreateOrderContext(orders, options),
-                }
-            );
-
-            for (const serverId of serverIds) {
-                const order = this.env.services.pos.orders.find(
-                    (order) => order.name === serverId.pos_reference
-                );
-
-                if (order) {
-                    order.server_id = serverId.id;
-                }
-            }
-
-            for (const order_id of order_ids_to_sync) {
-                this.db.remove_order(order_id);
-            }
-
-            this.failed = false;
-            this.set_synch("connected");
-            return serverIds;
-        } catch (error) {
-            console.warn("Failed to send orders:", orders);
-            if (error.code === 200) {
-                // Business Logic Error, not a connection problem
-                // Hide error if already shown before ...
-                if ((!this.failed || options.show_error) && !options.to_invoice) {
-                    this.failed = error;
-                    this.set_synch("error");
-                    throw error;
-                }
-            }
-            this.set_synch("disconnected");
-            throw error;
-        }
-    }
 });
